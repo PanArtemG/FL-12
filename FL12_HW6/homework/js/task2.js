f82bb41cefc12b214727715ceba347e81a592bea
PanArtemG <artem253510@gmail.com>
2019-12-24 00:40:42 +0200

    ref

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,15 +1,11 @@
 let a = prompt('Enter value side A:');
 let b = prompt('Enter value side B:');
 let c = prompt('Enter value side C:');
-
 let checkInputValues = isNaN(+a) || a === '' || isNaN(+b) || b === '' || isNaN(+c) || c === '';
-
 if (checkInputValues) {
     alert('input values should be ONLY numbers')
-
 } else if (a === "0" || b === "0" || c === "0") {
     alert('A triangle must have 3 sides with a positive definite length')
-
 } else {
     if (!(+a < (+b + +c) && +b < (+a + +c) && +c < (+a + +b))) {
         alert("Triangle doesn’t exist");
