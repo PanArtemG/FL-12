ca69435413aeef8e30796d6dc84bbcceb34b0adf
PanArtemG <artem253510@gmail.com>
2019-12-23 14:39:07 +0200

    add:task-2

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let a = prompt('Enter value side A:');
+let b = prompt('Enter value side B:');
+let c = prompt('Enter value side C:');
+
+let checkInputValues = isNaN(+a) && a !== '' || isNaN(+b) && b !== '' || isNaN(+c) && c !== '';
+
+if (checkInputValues) {
+    alert('input values should be ONLY numbers')
+} else {
+    if ( !(+a < (+b + +c) && +b < (+a + +c) && +c < (+a + +b)) ) {
+        alert("Triangle doesn’t exist");
+    } else if (a === b && a === c) {
+        console.log('Equilateral triangle');
+    } else if ( a === b && a !== c || b === c && b !== a) {
+        console.log('Isosceles triangle')
+    } else {
+        console.log('Scalene triangle');
+    }
+
+}
