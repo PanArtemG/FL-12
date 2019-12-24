a96a1a31be9a6553798b9cb961c328e9a5f6ac60
PanArtemG <artem253510@gmail.com>
2019-12-24 11:42:40 +0200

    fix:eslint

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -4,13 +4,13 @@ let c = +prompt("Enter number 'C'");
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('Invalid input data')
 } else {
-    let D = Math.pow(b, 2) - 4 * a * c;
+    let D = b * b - 4 * a * c;
     if ( D === 0 ) {
-        let x = (b * -1) / 2 * a;
+        let x = b * -1 / 2 * a;
         console.log(`x = ${x}`);
     } else if ( D > 0) {
-        let x1 = (b * -1) + Math.sqrt(D) / 2 * a;
-        let x2 = (b * -1) - Math.sqrt(D) / 2 * a;
+        let x1 = b * -1 + Math.sqrt(D) / 2 * a;
+        let x2 = b * -1 - Math.sqrt(D) / 2 * a;
         console.log(`x1 = ${x1} & x2 = ${x2}`);
     } else {
             console.log(`Discriminant = ${D} - The quadratic equation has no solution`);
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -4,11 +4,11 @@ let c = prompt('Enter value side C:');
 let checkInputValues = isNaN(+a) || a === '' || isNaN(+b) || b === '' || isNaN(+c) || c === '';
 if (checkInputValues) {
     alert('input values should be ONLY numbers')
-} else if (a === "0" || b === "0" || c === "0") {
+} else if (a === '0' || b === '0' || c === '0') {
     alert('A triangle must have 3 sides with a positive definite length')
 } else {
-    if (!(+a < (+b + +c) && +b < (+a + +c) && +c < (+a + +b))) {
-        alert("Triangle doesn’t exist");
+    if (!(+a < +b + +c && +b < +a + +c && +c < +a + +b)) {
+        alert('Triangle doesn’t exist');
     } else if (a === b && a === c) {
         console.log('Equilateral triangle');
     } else if (a === b && a !== c || b === c && b !== a) {
