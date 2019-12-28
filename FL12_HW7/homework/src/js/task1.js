1c154a73da279641ceb954c3549b3ae26c74897d
PanArtemG <artem253510@gmail.com>
2019-12-28 13:05:04 +0200

    ref:task1

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -8,80 +8,80 @@ const DB = {
         pass: 'AdminPass'
     }
 };
-let email = prompt('Enter yours email', 'user@gmail.com');
-let pass = null;
-let check = false;
 let minLengthEmail = 5;
 let minLengthPass = 6;
 
+
+let email = prompt('Enter yours email', 'user@gmail.com');
 
 if (!email) {
     alert('Canceled')
 } else if (email.length < minLengthEmail) {
     alert('I don\'t know any emails having name length less than 5 symbols')
-} else if (email.match(/@/)) {
-    if (email === DB.user.email) {
-        pass = prompt('Enter yours password', 'UserPass');
-        if (!pass) {
-            alert('Canceled')
-        } else if (pass === DB.user.pass) {
-            check = confirm('Do you want to change your password?');
-            if (!check) {
-                alert('You have failed the change.');
-            } else {
-                pass = prompt('Enter yours password', 'UserPass');
+} else if (email === DB.user.email) {
+    let pass = prompt('Enter yours password', 'UserPass');
+    if (!pass) {
+        alert('Canceled')
+    } else if (pass !== DB.user.pass) {
+        alert('Wrong password')
+    } else if (pass === DB.user.pass) {
+        let check = confirm('Do you want to change your password?');
+        if (!check) {
+            alert('You have failed the change.');
+        } else {
+            let pass = prompt('Enter yours password', 'UserPass');
+            if (!pass) {
+                alert('Canceled')
+            } else if (pass === DB.user.pass) {
+                pass = prompt('Enter NEW password', 'Password');
                 if (!pass) {
                     alert('Canceled')
-                } else if (pass === DB.user.pass) {
-                    pass = prompt('Enter NEW password', 'Password');
-                    if (!pass) {
-                        alert('Canceled')
-                    } else if (pass.length < minLengthPass) {
-                        alert('It’s too short password. Sorry.')
-                    } else if (pass.length >= minLengthPass) {
-                        let confirmPass = prompt('Enter your NEW password again', 'Password');
-                        if ( pass === confirmPass) {
-                            DB.user.pass = confirmPass;
-                            alert('You have successfully changed your password.')
-                        } else {
-                            alert('You wrote the wrong password.')
-                        }
-                    }
-                }
-            }
-        }
-    } else if (email === DB.admin.mail) {
-        pass = prompt('Enter yours password', 'UserPass');
-        if (!pass) {
-            alert('Canceled')
-        } else if (pass === DB.admin.pass) {
-            check = confirm('Do you want to change your password?');
-            console.log(check);
-            if (!check) {
-                alert('You have failed the change.');
-            } else {
-                pass = prompt('Enter yours password', 'AdminPass');
-                if (!pass) {
-                    alert('Canceled')
-                } else if (pass === DB.admin.pass) {
-                    pass = prompt('Enter NEW password', 'Password');
-                    if (!pass) {
-                        alert('Canceled')
-                    } else if (pass.length < minLengthPass) {
-                        alert('It’s too short password. Sorry.')
-                    } else if (pass.length >= minLengthPass) {
-                        let confirmPass = prompt('Enter your NEW password again', 'Password');
-                        if ( pass === confirmPass) {
-                            DB.admin.pass = confirmPass;
-                            alert('You have successfully changed your password.')
-                        } else {
-                            alert('You wrote the wrong password.')
-                        }
+                } else if (pass.length < minLengthPass) {
+                    alert('It’s too short password. Sorry.')
+                } else if (pass.length >= minLengthPass) {
+                    let confirmPass = prompt('Enter your NEW password again', 'Password');
+                    if (pass === confirmPass) {
+                        DB.user.pass = confirmPass;
+                        alert('You have successfully changed your password.')
+                    } else {
+                        alert('You wrote the wrong password.')
                     }
                 }
             }
         }
     }
+} else if (email === DB.admin.mail) {
+    pass = prompt('Enter yours password', 'AdminPass');
+    if (!pass) {
+        alert('Canceled')
+    } else if (pass !== DB.admin.pass) {
+        alert('Wrong password')
+    } else if (pass === DB.admin.pass) {
+        let check = confirm('Do you want to change your password?');
+        if (!check) {
+            alert('You have failed the change.');
         } else {
+            let pass = prompt('Enter yours password', 'AdminPass');
+            if (!pass) {
+                alert('Canceled')
+            } else if (pass === DB.admin.pass) {
+                pass = prompt('Enter NEW password', 'Password');
+                if (!pass) {
+                    alert('Canceled')
+                } else if (pass.length < minLengthPass) {
+                    alert('It’s too short password. Sorry.')
+                } else if (pass.length >= minLengthPass) {
+                    let confirmPass = prompt('Enter your NEW password again', 'Password');
+                    if (pass === confirmPass) {
+                        DB.admin.pass = confirmPass;
+                        alert('You have successfully changed your password.')
+                    } else {
+                        alert('You wrote the wrong password.')
+                    }
+                }
+            }
+        }
+    }
+} else {
     alert('I don’t know you')
 }
