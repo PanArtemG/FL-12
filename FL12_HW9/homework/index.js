0db3d9103b6aa154b6271ed0d3e4437640dc49dd
PanArtemG <artem253510@gmail.com>
2020-01-12 00:11:00 +0200

    add:competed_task

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -4,25 +4,19 @@ const convert = (...arg) => {
         (typeof (arg[i]) === 'string') ? arrConvertArg.push(+arg[i]) : arrConvertArg.push(arg[i] + '');
     return arrConvertArg
 };
-// convert('1', 2, 3, '4');
 
-//2
 const executeforEach = (arr, func) => {
     for (let i = 0; i < arr.length; i++) {
         func(arr[i]);
     }
 };
-// executeforEach([1,2,3], function first(el) {console.log(el * 2)});
 
-//3
 const mapArray = (arr, func) => {
     let transformArr = [];
     executeforEach(arr, el => transformArr.push(func(+el)));
     return transformArr;
 };
-// mapArray([2, '5', 8], function (el) {return el + 3}); // returns [5, 8, 11]
 
-//4
 const filterArray = (arr, func) => {
     let transformArr = [];
     let i = 0;
@@ -36,29 +30,23 @@ const filterArray = (arr, func) => {
     });
     return transformArr
 };
-// console.log(filterArray([2, 5, 8, 10, 11], function (el) {return el % 2 === 0})); // возвращает [2, 8]
 
-//5
-const flipOver = (str) => {
+const flipOver = str => {
     let flipOverString = '';
     for (let l = str.length - 1; l >= 0; l--) {
         flipOverString += str[l]
     }
     return flipOverString
 };
-// console.log(flipOver('hey world'));
 
-//6
-const makeListFromRange = (arr) => {
+const makeListFromRange = arr => {
     const listRange = [];
     for (let i = arr[0]; i <= arr[1]; i++) {
         listRange.push(i)
     }
     return listRange;
 };
-// console.log(makeListFromRange([5, 7]));
 
-//7
 const actors = [
     {name: 'tommy', age: 36},
     {name: 'lee', age: 28}
@@ -68,12 +56,26 @@ const getArrayOfKeys = (arr, key) => {
     executeforEach(arr, (el) => name.push(el[key]));
     return name
 };
-// console.log(getArrayOfKeys(actors, 'name')); // [‘tommy’, ‘lee’]
 
-//8
-const substitute = (arr) => {
+const substitute = arr => {
     let transformArr = [];
     mapArray(arr, el => (el <= 30) ? transformArr.push('*') : transformArr.push(el));
     return transformArr
 };
-// console.log(substitute([58, 14, 48, 2, 31, 29, 2,4,111])); // [58, '*', 48, '*', 31, '*']
+
+const date = new Date(2019, 0, 2);
+const getPastDay = (date, day) => {
+    const dayInMs = 86400000;
+    const convertDayInMs = day * dayInMs;
+
+    return (new Date(date.getTime() - convertDayInMs)).getDate()
+};
+
+const formatDate = date => {
+    const objData = {
+        day: date.getDate(),
+        month: date.getMonth() + 1,
+        year: date.getFullYear()
+    };
+    return `${objData.year}/${objData.month}/${objData.day}`
+};
