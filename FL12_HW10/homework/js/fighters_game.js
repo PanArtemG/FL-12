57b5e98bdca7297ead6a7fdab38b2f1539698823
PanArtemG <artem253510@gmail.com>
2020-01-20 16:00:44 +0200

    fullJob

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -47,8 +47,16 @@ class Fighter {
         return this.#wins
     }
 
+    addWin() {
+        return this.#wins += 1
+    }
+
     get getLosses() {
         return this.#losses
+    }
+
+    addLoss() {
+        return this.#losses += 1
     }
 
     get logCombatHistory() {
@@ -69,7 +77,6 @@ class Fighter {
 
     dealDamage(damage) {
         this.#hp -= damage;
-        this.#hp <= 0 ? this.#wins++ : null;
     }
 
     heal(healPoint) {
@@ -78,41 +85,37 @@ class Fighter {
         console.log(`Heal TRUE : HP = ${this.#hp}`)
     }
 
-    addWin() {
-        return  this.#wins += 1
-    }
 
-    addLoss() {
-        return  this.#losses += 1
-    }
 }
 
 
-let firsFighter = new Fighter({name: "Maximus", damage: 25, hp: 100, strength: 50, agility: 30});
-let secondFighters = new Fighter({name: "Titan", damage: 50, hp: 100, strength: 20, agility: 20});
-battle(firsFighter, secondFighters)
-
-
 function battle(firstFighter, secondFighter) {
-    if (firstFighter.getHealth && secondFighter.getHealth) {
+    if ((firstFighter.getHealth && secondFighter.getHealth) > 0) {
         firstFighter.attack(secondFighter);
         if (secondFighter.getHealth) {
             secondFighter.attack(firstFighter);
             if (firstFighter.getHealth) {
                 battle(firstFighter, secondFighter)
             } else {
-                secondFighter.addWin;
-                firstFighter.addLoss;
-                console.log(`${secondFighter.getName} WON! WINS : ${secondFighter.getWins}`)
+                secondFighter.addWin();
+                firstFighter.addLoss();
+                console.log(`${secondFighter.getName} WON! WINS : ${secondFighter.getWins}`);
             }
         } else {
-            console.log(`${firstFighter.getName} WON! WINS : ${firstFighter.getWins}`)
+            firstFighter.addWin();
+            secondFighter.addLoss();
+            console.log(`${firstFighter.getName} WON! WINS : ${firstFighter.getWins}`);
         }
+    } else if (firstFighter.getHealth <= 0) {
+        console.log(`${firstFighter.getName} are not ready! Restore HP`);
     } else {
-        new Error(`The fighters are not ready! ${firstFighter.getName} : ${firstFighter.getHealth} & ${secondFighter.getName} : ${secondFighter.getHealth} Restore HP`)
+        console.log(`${secondFighter.getName} are not ready! Restore HP`);
     }
 }
 
-// battle(firsFighter, secondFighters)
-// console.log(firsFighter.attack(secondFighters));
-// console.log(secondFighters.getHealth);
+
+let firsFighter = new Fighter({name: "Maximus", damage: 50, hp: 100, strength: 20, agility: 20});
+let secondFighters = new Fighter({name: "Titan", damage: 50, hp: 100, strength: 20, agility: 20});
+battle(firsFighter, secondFighters);
+
+
