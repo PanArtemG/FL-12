3e1078dc9a7c2ce80f698efd61bb2993d17fe179
PanArtemG <artem253510@gmail.com>
2020-01-28 23:17:28 +0200

    add:COMPLETED

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -3,10 +3,6 @@ const addPage = document.getElementById('addPage');
 const modifyPage = document.getElementById('modifyPage');
 const listTermContainer = document.getElementById('listTermContainer');
 
-
-const addNameInput = document.getElementById('addName');
-const addTermInput = document.getElementById('addTerm');
-
 const addTermsBtn = document.getElementById('addTermsBtn');
 const saveNewTermsBtn = document.getElementById('saveNewTermsBtn');
 const saveModifyBtn = document.getElementById('saveModifyBtn');
@@ -21,10 +17,7 @@ const hashList = {
     editHash: '#/modify/'
 };
 
-console.log(location.hash);
-
 loadListTerms();
-
 
 addTermsBtn.addEventListener('click', () => {
     location.hash = hashList.addHash;
@@ -33,34 +26,34 @@ addTermsBtn.addEventListener('click', () => {
 });
 
 saveNewTermsBtn.addEventListener('click', ev => {
-    createdNewTerm(ev)
+    getValueInput(ev)
 });
 
-cancelNewTermBtn.addEventListener('click', (ev) => {
-    closedBlock(ev)
+cancelNewTermBtn.addEventListener('click', ev => {
+    closedBlockListener(ev)
 });
 
-cancelModifyTermBtn.addEventListener('click', (ev) => {
-    closedBlock(ev)
+cancelModifyTermBtn.addEventListener('click', ev => {
+    closedBlockListener(ev)
 });
 
-// saveModifyBtn.addEventListener('click', () => {
-//
-// })
-
+saveModifyBtn.addEventListener('click', ev => {
+    let name = document.getElementById('modifyName').value;
+    let terms = document.getElementById('modifyTerms').value;
+    saveTerms(ev, name, terms)
+});
 
 function loadListTerms() {
-    // debugger
+    location.hash = hashList.mainHash;
     let BDTermsStorage = localStorage.getItem('BDTerms');
-    console.log(!!BDTermsStorage);
-    if (BDTermsStorage !== 'undefined') {
+
+    if (BDTermsStorage !== 'undefined' && BDTermsStorage) {
         BDTermsStorage = JSON.parse(BDTermsStorage);
         BDTermsStorage.forEach(el => {
             BDTerms.push(el)
         });
         createListItem();
     } else {
-
         listTermContainer.innerHTML = `<h2>LIST TERMS EMPTY</h2>`
     }
 }
@@ -75,6 +68,7 @@ function createListItem() {
             const itemBtnWrap = document.createElement('div');
             const itemStatusBtn = document.createElement('span');
             const itemModifyBtn = document.createElement('span');
+            const itemDeleteBtn = document.createElement('span');
 
             itemTitle.innerText = `Name : ${el.name}`;
             itemContent.innerText = `Term : ${el.term}`;
@@ -83,57 +77,76 @@ function createListItem() {
 
             itemBtnWrap.classList.add('btn-wrap');
 
-            itemStatusBtn.innerText = 'Change Status';
+            itemStatusBtn.innerText = 'Status';
             itemModifyBtn.innerText = 'modify';
+            itemDeleteBtn.innerText = 'delete';
 
             itemStatusBtn.setAttribute('status', `${el.status}`);
             itemModifyBtn.setAttribute('modify', 'true');
+            itemDeleteBtn.setAttribute('delete', 'true');
 
             itemStatusBtn.classList.add('btn', 'change-status');
             itemModifyBtn.classList.add('btn');
+            itemDeleteBtn.classList.add('btn');
 
             itemWrap.appendChild(itemTitle);
             itemWrap.appendChild(itemContent);
             itemWrap.appendChild(itemBtnWrap);
             itemBtnWrap.appendChild(itemStatusBtn);
             itemBtnWrap.appendChild(itemModifyBtn);
+            itemBtnWrap.appendChild(itemDeleteBtn);
 
-            itemWrap.classList.add(`${el.status ? 'status-true' : null}`);
+            itemStatusBtn.classList.add(`${el.status ? 'status-true' : null}`);
 
             listTermContainer.appendChild(itemWrap);
 
-            itemWrap.addEventListener('click', (ev) => {
+            itemWrap.addEventListener('click', ev => {
                 let target = ev.target;
                 let status = target.getAttribute('status');
                 let modify = target.getAttribute('modify');
+                let deleteBtn = target.getAttribute('delete');
+
+                let id = +ev.currentTarget.getAttribute('id');
 
                 if (status) {
-                    let id = +ev.currentTarget.getAttribute('id');
                     BDTerms.forEach(el => {
                         if (el._id === id) {
                             el.status = !el.status;
-                            itemWrap.classList.toggle('status-true');
+                            itemStatusBtn.classList.toggle('status-true');
                             const jsonBD = JSON.stringify(BDTerms);
                             localStorage.setItem('BDTerms', jsonBD);
                         }
                     });
                 } else if (modify) {
-                    closedBlock(ev)
+                    closedBlockListener(ev)
+                } else if (deleteBtn) {
+                    BDTerms.forEach((el, index) => {
+                        if (el._id === id) {
+                            itemWrap.classList.toggle('inactive');
+                            deleteTerms(index);
+                            const jsonBD = JSON.stringify(BDTerms);
+                            localStorage.setItem('BDTerms', jsonBD);
+                        }
+                    });
                 }
             })
         })
     }
 }
 
-function createdNewTerm(ev) {
-    let name = addNameInput.value;
-    let term = addTermInput.value;
-    if (name && term) {
+function getValueInput(ev) {
+    let name = document.getElementById('addName').value;
+    let terms = document.getElementById('addTerm').value;
+    saveTerms(ev, name, terms);
+}
+
+function saveTerms(ev, name, terms) {
+    if (name && terms) {
         const newTerm = {
             _id: new Date().getTime(),
             status: false,
             name: name,
-            term: term,
+            term: terms,
             date: new Date().getTime()
         };
         BDTerms.push(newTerm);
@@ -145,16 +158,16 @@ function saveStorage(BDTerms, ev) {
     const jsonBD = JSON.stringify(BDTerms);
     localStorage.setItem('BDTerms', jsonBD);
     resetInputValue();
-    closedBlock(ev);
+    closedBlockListener(ev);
     createListItem();
 }
 
 function resetInputValue() {
-    addNameInput.value = '';
-    addTermInput.value = '';
+    document.getElementById('addName').value = '';
+    document.getElementById('addTerm').value = '';
 }
 
-function closedBlock(ev) {
+function closedBlockListener(ev) {
     let btn = ev.target;
     let id = btn.getAttribute('id');
     let modify = btn.getAttribute('modify');
@@ -165,9 +178,9 @@ function closedBlock(ev) {
         resetInputValue();
     } else if (modify) {
         let id = +btn.parentElement.parentElement.getAttribute('id');
-        modifyData(id);
+        modifyTerms(id);
         modifyPage.classList.toggle('inactive');
-        location.hash = hashList.editHash;
+        location.hash = `${hashList.editHash}:${id}`;
     } else if (id === 'cancelModifyTermBtn') {
         modifyPage.classList.toggle('inactive');
         location.hash = hashList.mainHash;
@@ -175,18 +188,30 @@ function closedBlock(ev) {
         addPage.classList.toggle('inactive');
         location.hash = hashList.mainHash;
         resetInputValue();
+    } else if (id === 'saveModifyBtn') {
+        modifyPage.classList.toggle('inactive');
+        location.hash = hashList.mainHash;
+        resetInputValue();
     }
     mainPage.classList.toggle('inactive');
 }
 
-function modifyData(id) {
+function modifyTerms(id) {
     let modifyName = document.getElementById('modifyName');
-    let modifyTerm = document.getElementById('modifyTerm');
-    BDTerms.forEach(el => {
+    let modifyTerms = document.getElementById('modifyTerms');
+    BDTerms.forEach((el, index) => {
         if (el._id === id) {
             modifyName.value = el.name;
-            modifyTerm.value = el.term;
+            modifyTerms.value = el.term;
+            deleteTerms(index)
         }
     })
 }
 
+function deleteTerms(index) {
+    if (!index) {
+        BDTerms.splice(index, ++index);
+    } else {
+        BDTerms.splice(index, index);
+    }
+}
