64808a4c4b3da42062b054c87f3a59a763c90d6d
PanArtemG <artem253510@gmail.com>
2020-01-28 16:10:51 +0200

    add:createTerm

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,6 +1,8 @@
 const mainPage = document.getElementById('mainPage');
 const addPage = document.getElementById('addPage');
 const modifyPage = document.getElementById('modifyPage');
+const listTermContainer = document.getElementById('listTermContainer');
+
 
 const addNameInput = document.getElementById('addName');
 const addTermInput = document.getElementById('addTerm');
@@ -18,18 +20,55 @@ const hashList = {
     editHash: '#/modify/'
 };
 
-
-
-
 console.log(location.hash);
 
-saveNewTermsBtn.addEventListener('click', createdNewTerm);
+loadListTerms();
+
+function createListItem() {
+    if (BDTerms.length) {
+        listTermContainer.innerHTML = null;
+        BDTerms.map(el => {
+            const itemWrap = document.createElement('div');
+            const itemTitle = document.createElement('h3');
+            const itemContent = document.createElement('p');
+            const itemBtnWrap = document.createElement('div');
+            const itemStatusBtn = document.createElement('span');
+            const itemModifyBtn = document.createElement('span');
+
+            itemTitle.innerText = `Name : ${el.name}`;
+            itemContent.innerText = `Term : ${el.term}`;
+            itemWrap.setAttribute('id', `${el._id}`);
+            itemWrap.setAttribute('class', 'term-item');
+
+            itemBtnWrap.setAttribute('class', 'btn-wrap');
+
+            itemStatusBtn.innerText = 'Change Status';
+            itemModifyBtn.innerText = 'modify';
+
+            itemStatusBtn.setAttribute('class', 'btn');
+            itemModifyBtn.setAttribute('class', 'btn');
+
+            itemWrap.appendChild(itemTitle);
+            itemWrap.appendChild(itemContent);
+            itemWrap.appendChild(itemBtnWrap);
+            itemBtnWrap.appendChild(itemStatusBtn);
+            itemBtnWrap.appendChild(itemModifyBtn);
+
+            listTermContainer.appendChild(itemWrap)
+
+        })
+    } else {
+        listTermContainer.innerHTML = `<h2>LIST TERMS EMPTY</h2>`
+    }
+}
 
 addTermsBtn.addEventListener('click', () => {
     location.hash = hashList.addHash;
     mainPage.classList.toggle('inactive');
     addPage.classList.toggle('inactive');
 });
+
+saveNewTermsBtn.addEventListener('click', createdNewTerm);
 
 cancelNewTermBtn.addEventListener('click', () => {
     closedBlock()
@@ -40,7 +79,18 @@ cancelModifyTermBtn.addEventListener('click', () => {
 });
 
 
-function createdNewTerm (){
+function loadListTerms() {
+    let BDTermsStorage = localStorage.getItem('BDTerms');
+    if (BDTermsStorage) {
+        BDTermsStorage = JSON.parse(BDTermsStorage);
+        BDTermsStorage.map(el => {
+            BDTerms.push(el)
+        });
+    }
+    createListItem();
+}
+
+function createdNewTerm() {
     console.log('createdNewTerm');
     let name = addNameInput.value;
     let term = addTermInput.value;
@@ -50,15 +100,23 @@ function createdNewTerm (){
             status: false,
             name: name,
             term: term,
-            date : new Date().getTime()
+            date: new Date().getTime()
         };
         BDTerms.push(newTerm)
     }
     console.log(BDTerms);
     const jsonBD = JSON.stringify(BDTerms);
     localStorage.setItem('BDTerms', jsonBD);
-    closedBlock()
+    resetInputValue();
+    closedBlock();
+    createListItem();
 }
+
+function resetInputValue() {
+    addNameInput.value = '';
+    addTermInput.value = '';
+}
+
 function closedBlock() {
     mainPage.classList.toggle('inactive');
     if (location.hash === hashList.addHash) {
@@ -66,5 +124,6 @@ function closedBlock() {
     } else {
         modifyPage.classList.toggle('inactive');
     }
-    location.hash = hashList.mainHash
+    location.hash = hashList.mainHash;
+    resetInputValue()
 }
